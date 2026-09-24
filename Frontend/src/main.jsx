e1dcc76d6ed64070d11634845f358ad7ea21ac17
Deepanshu-8126 <deepanshu4@gmail.com>
2026-09-24 19:18:29 +0530

    Make root container mounting bulletproof to completely prevent React Error 299

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,8 +3,23 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+function mountApp() {
+  let rootElement = document.getElementById('root');
+  if (!rootElement) {
+    rootElement = document.createElement('div');
+    rootElement.id = 'root';
+    document.body.appendChild(rootElement);
+  }
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
+
+if (document.readyState === 'loading') {
+  document.addEventListener('DOMContentLoaded', mountApp);
+} else {
+  mountApp();
+}
+
